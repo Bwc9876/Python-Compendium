59dd9f27f3998d0b85263c2680d5ae90b96bee28
Ben C <bwc9876@gmail.com>
2022-02-17 06:53:37 -0800

    Added Capitalization

--- console/english.py
+++ console/english.py
@@ -2,12 +2,19 @@
 VOWELS = ('a', 'e', 'i', 'o', 'u', 'y')
 TRUE_VOWELS = tuple(VOWELS[:-1])
 
+ARTICLES = ('a', 'an', 'the')
+COORD_CONJUNCTIONS = ('for', 'and', 'nor', 'but', 'or', 'yet')
+
 __all__ = [
     "pluralize",
     "auto_plural",
     "number_of",
     "list_items",
-    "a_or_an"
+    "a_or_an",
+    "cap_first",
+    "cap_first_title",
+    "cap_first_all",
+    "title"
 ]
 
 def pluralize(word: str, plural_string="s", vowel_plural_string="es", y_plural_string="ies"):
@@ -46,6 +53,25 @@ def a_or_an(word: str, consider_acronyms=False):
     if word[0] in TRUE_VOWELS and (consider_acronyms is False or word.isupper() is False):
         return "an " + word
     return "a " + word
+
+
+def cap_first(word: str):
+    return word[0].upper() + word[1:]
+
+
+def cap_first_title(word: str):
+    if word.lower() in ARTICLES or word.lower() in COORD_CONJUNCTIONS or len(word) < 4:
+        return cap_first(word)
+    else:
+        return word
+
+
+def cap_first_all(phrase: str):
+    return ' '.join([cap_first(word) for word in phrase.split(" ")])
+
+
+def title(phrase: str):
+    return ' '.join([cap_first_title(word.lower()) for word in phrase.split(" ")])
 
 
 
